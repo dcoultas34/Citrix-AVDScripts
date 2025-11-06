88a7395d65fd4374ec5cbdbe6ad4ced747c1fd92
dcoultas34 <145005165+dcoultas34@users.noreply.github.com>
2025-11-06 10:10:46 +0000

    Add files via upload

---
+++ UpdateVisualStudio.ps1
@@ -0,0 +1,8 @@
+﻿$vsInstaller = "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe"
+
+if (Test-Path $vsInstaller) {
+    & $vsInstaller update --all --quiet --norestart
+    Write-Host "Visual Studio update started (quiet mode, no restart)."
+} else {
+    Write-Host "Visual Studio Installer not found!"
+}
